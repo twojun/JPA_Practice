8e89844887a366b470f0d14965c7150078a53cb0
yoowonjun <twojunspace@naver.com>
2024-02-04 21:13:24 +0900

    Initial commit

---
+++ src/main/java/jpa_basic_shop/jpa_basic_shop/JpaBasicShopApplication.java
@@ -0,0 +1,13 @@
+package jpa_basic_shop.jpa_basic_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaBasicShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaBasicShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jpa_basic_shop/jpa_basic_shop/JpaBasicShopApplicationTests.java
@@ -0,0 +1,13 @@
+package jpa_basic_shop.jpa_basic_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaBasicShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
