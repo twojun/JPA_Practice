d9b048f92a557fe6b1ac80ac1ecaf9d01eddd748
yoowonjun <twojunspace@naver.com>
2024-02-15 02:49:10 +0900

    Collection 조회 : OneToMany 최적화

--- src/main/java/jpa_basic_shop/jpa_basic_shop/dto/OrderItemDto.java
+++ src/main/java/jpa_basic_shop/jpa_basic_shop/dto/OrderItemDto.java
@@ -8,7 +8,7 @@ public class OrderItemDto {
 
     private String itemName;  // 상품명
     private int orderPrice;  // 주문 가격
-    private int count;       // 주문 수량 (?)
+    private int count;       // 주문 수량
 
     public OrderItemDto(OrderItem orderItem) {
         this.itemName = orderItem.getItem().getName();
